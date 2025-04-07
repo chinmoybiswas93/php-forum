8b7cb36ba7e2d07a33b58e7183ec6422471dda07
chinmoybiswas93 <chinmoy709@gmail.com>
2025-04-07 13:35:28 +0600

    added forum templates

---
+++ config/config.php
@@ -0,0 +1,39 @@
+<?php
+define('BASE_URL', "https://forum.dev:8890");
+
+// Database connection details
+$servername = "localhost"; // Use only the hostname
+$username = "root";
+$password = "root"; // Default MAMP password
+$dbname = "ff_forum";
+$port = 8889; // MAMP MySQL port
+$socket = "/Applications/MAMP/tmp/mysql/mysql.sock"; // MAMP MySQL socket path
+
+try {
+    // Create a PDO connection with the socket
+    $dsn = "mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8mb4;unix_socket=$socket";
+    $pdo = new PDO($dsn, $username, $password);
+
+    // Set PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    //chekc if the 'users' table exists, if not create a one with the same structure
+    $sql = "CREATE TABLE IF NOT EXISTS users (
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        avatar VARCHAR(255) DEFAULT 'default.jpg',
+        about VARCHAR(255) DEFAULT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
+    $pdo->exec($sql);
+
+    // users table with id, name, email, username, password, created_at
+
+} catch (PDOException $e) {
+    // Display the error message on the page
+    die("Database connection failed: " . $e->getMessage());
+}
---
+++ create.php
@@ -0,0 +1,43 @@
+<?php require 'includes/header.php' ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-8">
+			<div class="main-col">
+				<div class="block">
+					<h1 class="pull-left">Create A Topic</h1>
+					<h4 class="pull-right">A Simple Forum</h4>
+					<div class="clearfix"></div>
+					<hr>
+					<form role="form">
+						<div class="form-group">
+							<label>Topic Title</label>
+							<input type="text" class="form-control" name="title" placeholder="Enter Post Title">
+						</div>
+						<div class="form-group">
+							<label>Category</label>
+							<select class="form-control">
+								<option>Design</option>
+								<option>Development</option>
+								<option>Business & Marketing</option>
+								<option>Search Engines</option>
+								<option>Cloud & Hosting</option>
+							</select>
+						</div>
+						<div class="form-group">
+							<label>Topic Body</label>
+							<textarea id="body" rows="10" cols="80" class="form-control" name="body"></textarea>
+							<script>CKEDITOR.replace('body');</script>
+						</div>
+						<button type="submit" class="color btn btn-default">Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<?php require 'includes/sidebar.php' ?>
+		</div>
+	</div>
+</div><!-- /.container -->
+
+<?php require 'includes/footer.php' ?>
